46f71034337e9f0771d76e4a2715c6949e71d963
Udit <uditmalik34@gmail.com>
2023-09-10 19:07:28 +0530

    Exploring the World

--- src/components/atoms/Body/index.js
+++ src/components/atoms/Body/index.js
@@ -4,16 +4,25 @@ import Card from "../Card";
 
 const Body = () => {
   const [restaurantList, setRestaurantList] = useState(resObj);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredList, setFilteredList] = useState(resObj);
 
-  useEffect(async () => {
-    const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6078909&lng=77.3054884&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-    );
-    const response = await data.json();
-    setRestaurantList(
-      response?.data?.cards?.[2]?.card?.card?.gridElements?.infoWithStyle
-        ?.restaurants
-    );
+  useEffect(() => {
+    const fetachRestaurantList = async () => {
+      const data = await fetch(
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6078909&lng=77.3054884&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      );
+      const response = await data.json();
+      if (
+        response?.data?.cards?.[2]?.card?.card?.gridElements?.infoWithStyle
+          ?.restaurants
+      ) {
+        setRestaurantList(
+          response?.data?.cards?.[2]?.card?.card?.gridElements?.infoWithStyle
+            ?.restaurants
+        );
+      }
+    };
   }, []);
 
   const highRated = () => {
@@ -25,9 +34,32 @@ const Body = () => {
 
   return (
     <div className="body-container">
-      <button onClick={() => highRated()}>Higher Rated</button>
+      <div>
+        <div>
+          <input
+            type="text"
+            value={searchQuery}
+            onChange={(e) => {
+              setSearchQuery(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              const filterData = restaurantList.filter((item) => {
+                return item.info.name
+                  .toLowerCase()
+                  .includes(searchQuery.toLowerCase());
+              });
+              setFilteredList(filterData);
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <button onClick={() => highRated()}>Higher Rated</button>
+      </div>
       <div className="card-container">
-        {restaurantList.map((item) => (
+        {filteredList.map((item) => (
           <Card key={item.info.id} resData={item} />
         ))}
       </div>
--- src/components/atoms/Header/index.js
+++ src/components/atoms/Header/index.js
@@ -1,24 +1,30 @@
 import { LOGO_URL } from "../../../utils/constants";
+import { useState } from "react";
 
 const Header = () => {
-    return (
-      <div className="header">
-        <div className="logo-container">
-          <img
-            className="logo"
-            src={LOGO_URL}
-          />
-        </div>
-        <div className="nav">
-          <ul className="nav-items">
-            <li>Home</li>
-            <li>Services</li>
-            <li>Contact Us</li>
-            <li>Cart</li>
-          </ul>
-        </div>
+  const [btnText, setBtnText] = useState("Login");
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={LOGO_URL} />
       </div>
-    );
-  };
+      <div className="nav">
+        <ul className="nav-items">
+          <li>Home</li>
+          <li>Services</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+          <button
+            onClick={() => {
+              setBtnText((prev) => (prev === "Logout" ? "Login" : "Logout"));
+            }}
+          >
+            {btnText}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-  export default Header;
+export default Header;
